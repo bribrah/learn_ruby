3cfa6c6da5f758665840076d1c10c00e9af4d449
bribrah <cheesemas46@gmail.com>
2018-11-01 23:49:56 -0700

    finished couple more

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,55 @@
 #write your code here
+def echo(str)
+    return str
+end
+
+def shout(str)
+    return str.upcase
+end
+
+def repeat(str, num = 2)
+    repeat = ""
+    while num>0
+        if num > 1
+        repeat += str + " "
+        num -= 1
+    else
+        repeat += str       
+        num -= 1
+        end
+    end
+    return repeat
+end
+
+def start_of_word(str, num = 1)
+    i = 0
+    first_letters =""
+    while i<num
+        first_letters += str[i]
+        i += 1
+    end
+    return first_letters
+end
+
+def first_word(str)
+    first_word = ""
+    i = 0
+    while str[i] != " "
+        first_word+= str[i]
+        i+= 1
+    end
+    return first_word
+end
+
+def titleize(str)
+    words = str.split.map do |word|
+        if  %w(and over the).include?(word)
+            word
+        else
+            word.capitalize
+        end
+    end
+    words.first.capitalize!
+    str = words.join(" ")
+    return str
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,23 @@
-#write your code here
+def translate(str)
+    vowel_reached = false
+    str_array = str.split(" ")
+    i = 0
+    while i < str_array.length
+        current_word = str_array[i]
+        vowel_reached = false
+        while vowel_reached == false
+            if %w(a e i o u).include?(current_word[0]) and current_word[current_word.length-1] !="q"
+                current_word += "ay"
+                vowel_reached = true
+                str_array[i] = current_word
+                i+=1
+            else
+                first_letter = current_word[0]
+                current_word[0] = ""
+                current_word += first_letter
+            end
+        end
+    end
+    puts str_array
+    str = str_array.join(" ")
+end
