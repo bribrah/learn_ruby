909e568c3d6d2d8e6781849e9e3b6badb96162a2
bribrah <cheesemas46@gmail.com>
2018-11-01 20:39:49 -0700

    finished exercise 3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add (num1,num2)
+    return num1 + num2
+end
+
+def subtract (num1,num2)
+    return num1 - num2
+end
+
+def sum(array)
+    i = 0
+    sum= 0
+    while i < array.length
+        sum += array[i]
+        i+= 1
+    end
+    return sum
+end
+
+
