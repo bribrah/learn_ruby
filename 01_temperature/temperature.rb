76c32cd8dcdccfc4e8f3439931afef118d4dd4c1
bribrah <cheesemas46@gmail.com>
2018-11-01 20:33:17 -0700

    finished first two exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,17 @@
 #write your code here
+def ftoc(f_temp)
+    c_temp = (f_temp -32)* (5.0/9.0)
+    if c_temp > 100
+        c_temp = 100
+        end
+    return c_temp
+end
+
+def ctof (c_temp)
+    f_temp = c_temp * 9.0/5.0 + 32
+    if c_temp >= 100
+        f_temp = 212
+    end
+    return f_temp
+end
+     
