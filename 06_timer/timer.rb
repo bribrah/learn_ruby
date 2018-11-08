fedb7dd9483bd7ae71ca26e964f86e3a609e8b0e
bribrah <cheesemas46@gmail.com>
2018-11-07 20:15:22 -0800

    Finished all exercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,20 @@
-class Timer
-  #write your code here
+class Timer 
+  attr_accessor :seconds, :minutes, :hours
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours = 0
+  end
+
+  def time_string
+    while @seconds > 60
+      @seconds -= 60
+      @minutes += 1
+    end
+    while @minutes > 60
+      @minutes -= 60
+      @hours += 1
+    end
+    return "#{sprintf '%02d', @hours}:#{sprintf'%02d',@minutes}:#{sprintf'%02d',@seconds}"
+  end
 end
