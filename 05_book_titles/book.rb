18cd34444c9cfd8455f66625401399ec27f567b3
bribrah <cheesemas46@gmail.com>
2018-11-07 20:15:48 -0800

    finished all exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+    def title
+        @title
+    end
+    def title= (str)
+        words = str.split
+        new_title = ""
+        words.each_with_index do |word, index|
+            if (word.length > 3 or index == 0 or word == "i") and %w(the a an)(word) 
+                new_title = new_title + word.capitalize + " "
+            else
+                new_title = new_title + word + " "
+            end
+            @title = new_title.chop
+        end
+    end
 end
